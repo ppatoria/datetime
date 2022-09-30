182c87321915ff033030500c670c93d5d76d0081
ppatoria <ppatoria@gmail.com>
2022-09-30 03:58:27 -0400

    fixed test and catch2

--- tests/UtcDateTimeTests.cpp
+++ tests/UtcDateTimeTests.cpp
@@ -1,14 +1,15 @@
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch_all.hpp"
+
 #include <dt/local_date_time.hpp>
 #include <dt/utc_date_time.hpp>
 #include <iostream>
 
-int main()
+TEST_CASE("test", "[test]")
 {
     auto now_utc = dt::utc_date_time::now().toString();
     std::cout << now_utc << std::endl;
 
     auto now_local = dt::local_date_time::now().toString();
     std::cout << now_local << std::endl;
-
-    return 0;
 }
