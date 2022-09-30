01f29037fbb693aefce2b69800e80c637c3aca2c
ppatoria <ppatoria@gmail.com>
2022-09-30 16:28:19 -0400

    improvement

--- dt/date.hpp
+++ dt/date.hpp
@@ -1,13 +1,47 @@
 #pragma once
 
 #include <boost/date_time/gregorian/gregorian.hpp>
+#include <stdexcept>
 #include <string>
+#include <string_view>
 
 namespace dt {
 
-using namespace boost::gregorian;
-
 class date : public boost::gregorian::date {
+private:
+    void to_string(std::string& s, const char* format) const
+    {
+        if (NULL == format || 0 == *format) {
+            return to_string(s);
+        }
+
+        try {
+            boost::gregorian::date_facet facet;
+            facet.format(format);
+
+            std::ostringstream str;
+            str.exceptions(
+                std::ios_base::failbit); // throw exception when parsing fails
+
+            std::ostreambuf_iterator<char> ost(str);
+            facet.put(ost, str, ' ', *this);
+
+            s = str.str();
+        } catch (const std::out_of_range& e) {
+            std::runtime_error("Failed to format <" + to_string() + "> " + "using format <" + format + "> " + "with error <" + e.what() + ">");
+        }
+    }
+
+    void to_string(std::string& s) const
+    {
+        if (empty()) {
+            s.clear();
+            return;
+        }
+
+        s = to_iso_extended_string(value());
+    }
+
 public:
     using parent_type = boost::gregorian::date;
     using year_type = parent_type::year_type;
@@ -77,30 +111,49 @@ public:
         return s;
     }
 
-    void to_string(std::string& s) const
+    /**
+     * See "date Time Input/Output" in boost documentation for a detailed
+     * description of various formats
+     **/
+    date& from_string(const char* value, const char* format = "")
     {
-        if (empty()) {
-            s.clear();
-            return;
+        dt::date date;
+        try {
+            boost::gregorian::date_input_facet facet;
+            if (NULL == format || 0 == *format) {
+                facet.set_iso_extended_format();
+            } else {
+                facet.format(format);
+            }
+
+            std::istringstream s(value);
+            s.exceptions(std::ios_base::failbit); // throw exception when parsing fails
+
+            std::istreambuf_iterator<char> i(s), e;
+            facet.get(i, e, s, date);
+        } catch (const std::out_of_range& e) {
+            std::runtime_error("Failed to parse <" + std::string(value) + "> as Date "
+                + "using format <" + format + "> "
+                + "with error <" + e.what() + ">");
         }
 
-        s = to_iso_extended_string(value());
+        *this = date;
+
+        return *this;
     }
 
-    // See "date Time Input/Output" in boost documentation for a detailed
-    // description of various formats
-    date& fromString(const char* value, const char* format = "");
-
-    date& fromString(const std::string& s, const char* format = "")
+    dt::date& from_string(const std::string& s, const char* format = "")
     {
-        return fromString(s.c_str(), format);
+        return from_string(s.c_str(), format);
     }
-
-    void to_string(std::string& s, const char* format) const;
 
     friend std::ostream& operator<<(std::ostream& os, const date& value)
     {
         return os << value.to_string();
     }
 };
+static_assert(std::is_trivially_copyable<dt::date::parent_type>::value,
+    "Oops, why dt::date::parent_type is not trivially copyable?!");
+static_assert(std::is_trivially_copyable<dt::date>::value,
+    "Oops, why dt::date is not trivially copyable?!");
 } // namespace dt
