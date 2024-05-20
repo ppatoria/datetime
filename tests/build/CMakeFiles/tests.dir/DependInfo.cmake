dd778e7168ecdc4a9141e6dc17f152404d12c3e6
ppatoria <ppatoria@gmail.com>
2024-05-19 22:28:27 -0400

    latest

---
+++ tests/build/CMakeFiles/tests.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ppatoria/dev/datetime/tests/date_tests.cpp" "CMakeFiles/tests.dir/date_tests.cpp.o" "gcc" "CMakeFiles/tests.dir/date_tests.cpp.o.d"
+  "/home/ppatoria/dev/datetime/tests/local_date_time_tests.cpp" "CMakeFiles/tests.dir/local_date_time_tests.cpp.o" "gcc" "CMakeFiles/tests.dir/local_date_time_tests.cpp.o.d"
+  "/home/ppatoria/dev/datetime/tests/time_duration_tests.cpp" "CMakeFiles/tests.dir/time_duration_tests.cpp.o" "gcc" "CMakeFiles/tests.dir/time_duration_tests.cpp.o.d"
+  "/home/ppatoria/dev/datetime/tests/utc_date_time_tests.cpp" "CMakeFiles/tests.dir/utc_date_time_tests.cpp.o" "gcc" "CMakeFiles/tests.dir/utc_date_time_tests.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ppatoria/dev/datetime/tests/build/_deps/catch2-build/src/CMakeFiles/Catch2WithMain.dir/DependInfo.cmake"
+  "/home/ppatoria/dev/datetime/tests/build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ tests/build/CMakeFiles/tests.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tests.dir/date_tests.cpp.o"
+  "CMakeFiles/tests.dir/date_tests.cpp.o.d"
+  "CMakeFiles/tests.dir/local_date_time_tests.cpp.o"
+  "CMakeFiles/tests.dir/local_date_time_tests.cpp.o.d"
+  "CMakeFiles/tests.dir/time_duration_tests.cpp.o"
+  "CMakeFiles/tests.dir/time_duration_tests.cpp.o.d"
+  "CMakeFiles/tests.dir/utc_date_time_tests.cpp.o"
+  "CMakeFiles/tests.dir/utc_date_time_tests.cpp.o.d"
+  "tests"
+  "tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
